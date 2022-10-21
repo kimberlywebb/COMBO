d2aa6e20402fcf5118a8c302090c63c990f4a53d
kimhochstedler <kimhochstedler@gmail.com>
2022-10-21 15:51:02 -0400

    added ifesle to w_j in case of divide by 0

--- R/w_j.R
+++ R/w_j.R
@@ -80,7 +80,7 @@ w_j <- function(ystar_matrix, pistar_matrix, pi_matrix, sample_size, n_cat){
                                          byrow = FALSE)
   obs_Y_matrix_repped_1 <- matrix(rep(c(ystar_matrix), each = n_cat),
                                   nrow = n_cat * sample_size, byrow = TRUE)
-  weight_not_summed_1 <- obs_Y_matrix_repped_1 * (pistar_pi_1 / suml_pistar_pi_denominator_1)
+  weight_not_summed_1 <- obs_Y_matrix_repped_1 * (pistar_pi_1 / ifelse(suml_pistar_pi_denominator_1 == 0, .00000001, suml_pistar_pi_denominator_1))
 
   weight_1 <- matrix(NA, nrow = sample_size, ncol = n_cat)
   for(i in 1:sample_size){
